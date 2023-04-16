e9c87674255665b773f47b1a8e8b9d8bdc0c0093
roberto <robertointerface@gmail.com>
2023-04-16 14:59:35 +0100

    implement rest api pokemon data extractor
    implement class that uses pokemon rest api to get pokemons data

--- src/data_extractors/api_extractor.py
+++ src/data_extractors/api_extractor.py
@@ -1,7 +1,9 @@
+import json
+from httpx import AsyncClient
 from src.constants import POKEMON_API_URL
 
 
-class AsyncPokemonApiExtractor:
+class AsyncPokemonRestApiConsumer:
 
     def __init__(self, pokemon_name: str):
         self._pokemon_name = pokemon_name
@@ -10,6 +12,14 @@ class AsyncPokemonApiExtractor:
     def build_api_call(self) -> str:
         return f'{POKEMON_API_URL}/{self._pokemon_name}'
 
-    async def get_pokemon_data(self):
-        pass
+    @property
+    def result(self) -> dict:
+        if self.api_result is None:
+            raise ValueError("pokemon result was not extracted yet")
+        return self.api_result
 
+    async def get_pokemon_data(self, client: 'AsyncClient' = None):
+        resp = await client.get(self.build_api_call(),
+                                timeout=6.1,
+                                follow_redirects=True)
+        self.api_result = json.loads(resp.read())
---
+++ src/test/test_data_extractors/test_pokemon_rest_api_extractor.py
@@ -0,0 +1,31 @@
+import pytest
+from pytest_mock import mocker
+from httpx import AsyncClient
+from src.data_extractors.api_extractor import AsyncPokemonRestApiConsumer
+from src.constants import POKEMON_API_URL
+from collections import abc
+
+@pytest.mark.unit
+class TestAsyncPokemonApiExtractor:
+
+    POKEMON_NAME = 'pikachu'
+
+    def test_property_build_api_call_returns_correct_value(self):
+        api_consumer = AsyncPokemonRestApiConsumer(self.POKEMON_NAME)
+        rest_api_url = api_consumer.build_api_call()
+        expected_api_url = f'{POKEMON_API_URL}/{self.POKEMON_NAME}'
+        assert rest_api_url == expected_api_url
+
+    @pytest.mark.asyncio
+    async def test_get_pokemon_data_method_async_client_is_awaited(self,
+                                                                   mock_AsyncClient_get):
+        api_consumer = AsyncPokemonRestApiConsumer(self.POKEMON_NAME)
+        async with AsyncClient() as client:
+            await api_consumer.get_pokemon_data(client)
+            assert isinstance(api_consumer.api_result, abc.MutableMapping)
+
+    def test_HttpError_is_raised_if_rest_api_returns_non_2xx_response(self):
+        pass
+
+    def test_get_pokemon_data_method_saves_result_on_attribute_api_result(self):
+        pass
